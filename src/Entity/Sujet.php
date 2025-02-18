09594497203d5e43418188fa903727117b478230
Amanda <amanda.heryprieto@etu.unilim.fr>
2025-02-18 20:53:06 +0200

    Ciel UE L316

---
+++ src/Entity/Sujet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SujetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SujetRepository::class)]
+class Sujet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
